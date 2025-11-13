1aa259197022edb13892b6e15e1fda62aa043c05
Pramod Patil <pramodpatil5366@gmail.com>
2025-11-13 12:01:31 +0000

    implemented ticket booking function

--- 13-11-2025/multi_thread_ride_booking.c
+++ 13-11-2025/multi_thread_ride_booking.c
@@ -11,12 +11,28 @@ int ticket[5] = {0}; //0 for empty_seat, 1 for booked_seat
 pthread_mutex_t mutex;  //Forlock and unlocking the critical section 
 
 //function for ticket booking
-void *ticket_book(){
+void *ticket_book(void* arg){
+	int seat_no = *((int *)arg);
+	pthread_mutex_lock(&mutex);
+	 int found = 0;
+	for(int i=0; i<5; i++){
+		if(ticket[i] == 0){
+			ticket[i] = 1; //marking seat as booked
+			booked_seats++;
+			available_seats--;
+			printf("Seat number %d booked successfully!\n", i+1);
+			found = 1;
+			break;
+		}
+	}
+	if(!found){
+		printf("No available seats to book.\n");
+	}
 	return NULL;
 }
 
 //function for ticket cancelling
-void ticket_cancel(){
+void *ticket_cancel(){
 	return NULL;
 }
 
